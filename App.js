37a15998ec1382cadbe21180307143641367c043
KangSuzy <astnwl321@gmail.com>
2022-06-21 16:20:33 +0900

    레이아웃 작업 마무리!

--- App.js
+++ App.js
@@ -15,9 +15,9 @@ export default class App extends React.Component {
         </View>
 
         <View style={styles.ddayView}>
-        <Text> 수능까지 </Text>
-        <Text> D-123 </Text>
-        <Text> 2022년 06월 21일 </Text>
+        <Text style={styles.titleText}> 수능까지 </Text>
+        <Text style= {styles.ddayText}> D-123 </Text>
+        <Text style={styles.dateText}> 2022년 06월 21일 </Text>
         </View>
 
         <View style={styles.chatView}>
@@ -41,13 +41,37 @@ const styles = StyleSheet.create({
     settingView: {
         flex: 1,
         backgroundColor: 'red',
+        justifyContent: 'center',
+        alignItems: 'flex-end',
+        marginRight: '1%',
     },
     ddayView: {
         flex: 6,
         backgroundColor: 'green',
+        justifyContent: 'center',
+        alignItems: 'center',
     },
     chatView: {
         flex: 6,
         backgroundColor: 'blue',
     },
+     titleText:{
+        alignSelf: 'flex-end',
+        fontSize: 36,
+        fontWeight: 'bold',
+        color: '#4A4A4A',
+        marginRight: '15%',
+     },
+     ddayText: {
+        fontSize: 100,
+        fontWeight: 'bold',
+        color: '#4A4A4A',
+     },
+     dateText: {
+        alignSelf: 'flex-start',
+        fontSize: 21,
+        fontWeight: 'bold',
+        color: '#4A4A4A',
+        marginLeft: '15%',
+     },
 });
