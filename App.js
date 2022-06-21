f88d339816736e910d1f9e6c3c338f411b2f53c5
KangSuzy <astnwl321@gmail.com>
2022-06-21 14:47:23 +0900

    레이아웃 구성 : setting component

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 //App.js
 import React from 'react';
-import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
 
 
 export default class App extends React.Component {
@@ -9,11 +9,15 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <View style={styles.settingView}>
-
+        <TouchableOpacity>
+        <Image source={require('./icon/setting.png')}/>
+        </TouchableOpacity>
         </View>
+
         <View style={styles.ddayView}>
-
+        
         </View>
+
         <View style={styles.chatView}>
 
         </View>
